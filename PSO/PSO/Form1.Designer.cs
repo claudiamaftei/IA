02225ef48a3a5cc8587e97010f695521ce4bc2a9
claudiamaftei <61314136+claudiamaftei@users.noreply.github.com>
2021-06-29 11:06:12 +0300

    Add files via upload

---
+++ PSO/PSO/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PSO
+{
+    partial class Particle_Swarm_Optimization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generate_input = new System.Windows.Forms.Button();
+            this.find_the_path = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.diagramView1 = new MindFusion.Diagramming.WinForms.DiagramView();
+            this.diagram1 = new MindFusion.Diagramming.Diagram();
+            this.reset_graph = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generate_input
+            // 
+            this.generate_input.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.generate_input.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.generate_input.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.generate_input.FlatAppearance.BorderSize = 2;
+            this.generate_input.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSeaGreen;
+            this.generate_input.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generate_input.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.generate_input.Location = new System.Drawing.Point(185, 293);
+            this.generate_input.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.generate_input.Name = "generate_input";
+            this.generate_input.Size = new System.Drawing.Size(147, 50);
+            this.generate_input.TabIndex = 5;
+            this.generate_input.Text = "Generate input";
+            this.generate_input.UseMnemonic = false;
+            this.generate_input.UseVisualStyleBackColor = false;
+            this.generate_input.Click += new System.EventHandler(this.generate_input_Click);
+            // 
+            // find_the_path
+            // 
+            this.find_the_path.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.find_the_path.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.find_the_path.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.find_the_path.FlatAppearance.BorderSize = 2;
+            this.find_the_path.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSeaGreen;
+            this.find_the_path.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.find_the_path.Location = new System.Drawing.Point(397, 293);
+            this.find_the_path.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.find_the_path.Name = "find_the_path";
+            this.find_the_path.Size = new System.Drawing.Size(148, 50);
+            this.find_the_path.TabIndex = 6;
+            this.find_the_path.Text = "Find the path";
+            this.find_the_path.UseVisualStyleBackColor = false;
+            this.find_the_path.Click += new System.EventHandler(this.find_the_path_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox1.Location = new System.Drawing.Point(185, 371);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(569, 70);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // diagramView1
+            // 
+            this.diagramView1.BackColor = System.Drawing.Color.LightCyan;
+            this.diagramView1.Diagram = this.diagram1;
+            this.diagramView1.LicenseKey = null;
+            this.diagramView1.Location = new System.Drawing.Point(185, 54);
+            this.diagramView1.Name = "diagramView1";
+            this.diagramView1.Size = new System.Drawing.Size(569, 204);
+            this.diagramView1.TabIndex = 8;
+            this.diagramView1.Text = "diagramView1";
+            this.diagramView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.diagramView1_MouseDown);
+            this.diagramView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.diagramView1_MouseUp);
+            // 
+            // diagram1
+            // 
+            this.diagram1.TouchThreshold = 0F;
+            // 
+            // reset_graph
+            // 
+            this.reset_graph.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.reset_graph.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.reset_graph.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.reset_graph.FlatAppearance.BorderSize = 2;
+            this.reset_graph.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSeaGreen;
+            this.reset_graph.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reset_graph.Location = new System.Drawing.Point(606, 293);
+            this.reset_graph.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.reset_graph.Name = "reset_graph";
+            this.reset_graph.Size = new System.Drawing.Size(148, 50);
+            this.reset_graph.TabIndex = 9;
+            this.reset_graph.Text = "Reset Graph";
+            this.reset_graph.UseVisualStyleBackColor = false;
+            this.reset_graph.Click += new System.EventHandler(this.reset_graph_Click);
+            // 
+            // Particle_Swarm_Optimization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PSO.Properties.Resources.unnamed;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(939, 490);
+            this.Controls.Add(this.reset_graph);
+            this.Controls.Add(this.diagramView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.find_the_path);
+            this.Controls.Add(this.generate_input);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 8.25F);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Particle_Swarm_Optimization";
+            this.Text = "ParticleSwarmOptimization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button generate_input;
+        private System.Windows.Forms.Button find_the_path;
+        private System.Windows.Forms.TextBox textBox1;
+        private MindFusion.Diagramming.WinForms.DiagramView diagramView1;
+        private MindFusion.Diagramming.Diagram diagram1;
+        private System.Windows.Forms.Button reset_graph;
+    }
+}
+
---
+++ PSO/PSO/Form1.cs
@@ -0,0 +1,167 @@
+﻿using MindFusion.Diagramming;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Brush = MindFusion.Drawing.Brush;
+using System.Windows.Input;
+namespace PSO
+{
+
+    public partial class Particle_Swarm_Optimization : Form
+    {
+        private int nr_links = 0;
+        private static int nrOfNodes = 6;
+        RectangleF[] nodeBounds = new RectangleF[nrOfNodes];
+        private int first_nod;
+        private int[,] adiacent;
+        private Label[] label_position;
+        private Label[] label_costs;
+        private PSO pso = new PSO();
+        private void Initializare()
+        {
+
+            diagram1.DefaultShape = Shapes.Ellipse;
+            diagram1.ClearAll();
+
+
+
+            nodeBounds[0] = new RectangleF(8, 20, 10, 10);
+            nodeBounds[1] = new RectangleF(48, 5, 10, 10);
+            nodeBounds[2] = new RectangleF(48, 35, 10, 10);
+            nodeBounds[3] = new RectangleF(88, 5, 10, 10);
+            nodeBounds[4] = new RectangleF(88, 35, 10, 10);
+            nodeBounds[5] = new RectangleF(128, 20, 10, 10);
+            ShapeNode[] nod = new ShapeNode[6];
+            if (label_position != null)
+            {
+                for (int i = 0; i < label_position.Length; ++i)
+                {
+                    diagramView1.Controls.Remove(label_position[i]);
+                }
+            }
+            if (label_costs != null)
+            {
+                for (int i = 0; i < label_costs.Length; ++i)
+                {
+                    diagramView1.Controls.Remove(label_costs[i]);
+                }
+            }
+            for (int i = 0; i < nrOfNodes; ++i)
+            {
+                nod[i] = diagram1.Factory.CreateShapeNode(nodeBounds[i]);
+                nod[i].Brush = new MindFusion.Drawing.SolidBrush(Color.FromArgb(227, 204, 255));
+            }
+            label_position = new Label[nrOfNodes];
+            adiacent = new int[nrOfNodes, nrOfNodes];
+            first_nod = -1;
+            nr_links = 0;
+            
+
+        }
+        public Particle_Swarm_Optimization()
+        {
+            InitializeComponent();
+            Initializare();
+
+
+
+        }
+        private void diagramView1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            int seccond_node=-1 ;
+            for (int i = 0; i < nrOfNodes; i++)
+            {
+                var cursor_poz = diagramView1.PointerCursor;
+                if ((double)e.X/3.8  >= nodeBounds[i].X &&(double) e.X/3.8  <= nodeBounds[i].X + nodeBounds[i].Width
+                    && (double)e.Y/3.8 >= nodeBounds[i].Y && (double)e.Y/3.8 <= nodeBounds[i].Y + nodeBounds[i].Height)
+                {
+                    
+                    seccond_node = i;
+                    if(first_nod!=-1)
+                    {
+                        adiacent[first_nod, seccond_node] = 1;
+                        
+                        nr_links++;
+                    }
+                    break;
+                }
+                
+            }
+            first_nod = -1;
+        }
+        private void diagramView1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            for(int i=0;i<nrOfNodes;i++)
+            {
+                if ((double)e.X / 3.8 >= nodeBounds[i].X && (double)e.X / 3.8 <= nodeBounds[i].X + nodeBounds[i].Width
+                    && (double)e.Y / 3.8 >= nodeBounds[i].Y && (double)e.Y / 3.8 <= nodeBounds[i].Y + nodeBounds[i].Height)
+                {
+
+                    first_nod = i;
+                    break;
+                }
+                
+            }
+        }
+
+        private void generate_input_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            label_costs = new Label[nr_links];
+            int cnt = 0;
+            for (int i = 0; i < nrOfNodes; ++i)
+            {
+                
+                label_position[i] = new Label();
+                label_position[i].Text = i.ToString();
+                label_position[i].Font = new Font(label_position[i].Font.FontFamily, 15);
+                label_position[i].Location = new Point((int)(nodeBounds[i].X*3.8+7), (int)(nodeBounds[i].Y*3.8+7));
+                label_position[i].Visible = true;
+                label_position[i].Size = new Size(23,23);
+                label_position[i].BackColor = Color.FromArgb(227, 204, 255);
+                diagramView1.Controls.Add(label_position[i]);
+
+                for(int j=0;j<nrOfNodes;++j)
+                {
+                    if(adiacent[i,j]==1)
+                    {
+                        adiacent[i, j] = r.Next(1,11);
+                        label_costs[cnt] = new Label();
+                        label_costs[cnt].Text = adiacent[i, j].ToString();
+                        label_costs[cnt].Font = new Font(label_costs[cnt].Font.FontFamily, 15);
+                        label_costs[cnt].Location = new Point((int)((2*nodeBounds[i].X+nodeBounds[j].X)/3 * 3.8 + 7), (int)((2*nodeBounds[i].Y+nodeBounds[j].Y)/3 * 3.8 + 7));
+                        label_costs[cnt].Visible = true;
+                        label_costs[cnt].Size = new Size(23, 23);
+                        label_costs[cnt].BackColor = Color.White;
+                        diagramView1.Controls.Add(label_costs[cnt]);
+                        cnt++;
+                    }
+                }
+            }
+
+        }
+
+        private void reset_graph_Click(object sender, EventArgs e)
+        {
+            Initializare();
+            textBox1.Clear();
+        }
+
+        private void find_the_path_Click(object sender, EventArgs e)
+        {
+            pso.ParticleSwarm(6,adiacent);
+            var list = pso.optimizare_roi();
+            foreach (double val in list)
+                textBox1.Text += val.ToString()+"-->";
+
+            textBox1.Text += "STOP";
+
+        }
+    }
+}
